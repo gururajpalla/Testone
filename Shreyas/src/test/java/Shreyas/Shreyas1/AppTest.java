5468579fa1b8ade9f0508fe7b3b5f477eb186e94
gururaj <gururajpalla@gmail.com>
2019-08-29 23:45:40 +0530

    first commit

---
+++ Shreyas/src/test/java/Shreyas/Shreyas1/AppTest.java
@@ -0,0 +1,16 @@
+package Shreyas.Shreyas1;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    
+{
+  public static void main(String[] args) {
+System.out.println("shreyas.............");
+}
+}
